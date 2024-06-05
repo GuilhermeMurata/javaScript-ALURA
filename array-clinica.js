86d0fb61a029d4c748ef453677ad4739799ab69c
Guilherme Murata <123330836+GuilhermeMurata@users.noreply.github.com>
2024-06-05 16:42:33 -0300

    Add clinica

---
+++ array-clinica.js
@@ -0,0 +1,16 @@
+const clinica = [];
+
+const lobo = "lobo";
+const cao = "cao";
+const raposa = "raposa";
+
+clinica.push(lobo, cao, raposa);
+
+console.log(clinica);
+
+clinica.shift();
+console.log(clinica);
+clinica.shift();
+console.log(clinica);
+clinica.shift();
+console.log(clinica);
